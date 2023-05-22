3736afcebf7a0c40cb6862ba60f6c9081a7261d3
MariaRipley <roblesb.maria@gmail.com>
2023-05-22 17:53:57 +0200

    Add practice 2 from lesson 2.4.2

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Ejercicio 1
+
 const button = document.querySelector('.button');
 const userName = document.querySelector('.name');
 let messageGreetings = document.querySelector('.greetings');
@@ -8,3 +10,14 @@ let messageGreetings = document.querySelector('.greetings');
 button.addEventListener ('click', () => {
     messageGreetings.innerHTML = 'Hola ' + userName.value;
 })
+
+//Ejercicio 2
+
+const changingButton = document.querySelector('.button2');
+
+//addEventListener al hacer click en el botón
+//añadir o quitar la clase .button2-style al hacer click
+changingButton.addEventListener('click', () => {
+    changingButton.classList.toggle('style');
+})
+
